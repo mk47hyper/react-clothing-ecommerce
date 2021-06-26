eb596d13939ce600e86702a7253080b3dfd693e5
mk47hyper <mkhyper17@gmail.com>
2021-06-26 11:24:14 +0100

    Created directory and menu components.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Directory;
